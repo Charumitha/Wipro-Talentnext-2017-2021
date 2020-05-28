ee85d616594f47d00b6613ddf13dee99963ea750
Charumitha <65939725+Charumitha@users.noreply.github.com>
2020-05-29 00:15:11 +0530

    Add files via upload

---
+++ return_sec_last.java
@@ -0,0 +1,35 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.*;
+public class Main{
+    public static void main(String args[])
+    {
+        Scanner s= new Scanner(System.in);
+        int a,c;
+        System.out.println("enter the number");
+        a= s.nextInt();
+        
+        if(a>9)
+        {
+            c=a/10;
+            System.out.println(+c);
+        }
+        else if(a>99)
+        {
+            c=a/100;
+            System.out.println(+c);
+        }
+        
+        else
+            System.out.println("0");
+            
+        
+    }
+    
+}
