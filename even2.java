76c7d9b69ca364124b0a74a884fdc024ce71b499
Charumitha <65939725+Charumitha@users.noreply.github.com>
2020-05-29 01:07:40 +0530

    Add files via upload

---
+++ even2.java
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/		import java.util.*;
+import java.util.*;
+public class Main{
+    public static void main(String args[])
+    {
+        Scanner s= new Scanner(System.in);
+        int a;
+        System.out.println("enter the number");
+        a= s.nextInt();
+        
+        
+        if(a%2==0)
+        {
+            System.out.println(2);
+        }
+        else
+        {
+            System.out.println(0);
+        }
+    }
+}
+
+ 
