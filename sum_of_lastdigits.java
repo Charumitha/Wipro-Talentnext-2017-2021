a88ff06ca21889be0ac3da92575263fb1e3ec353
Charumitha <65939725+Charumitha@users.noreply.github.com>
2020-05-29 00:48:30 +0530

    Add files via upload

---
+++ sum_of_lastdigits.java
@@ -0,0 +1,33 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.*;
+public class Main{
+    public static void main(String args[])
+    {
+        Scanner s= new Scanner(System.in);
+        int a,c,b,d,sum;
+        System.out.println("enter the number");
+        a= s.nextInt();
+        b=s.nextInt();
+        
+        c=a%10;
+        d=b%10;
+        
+        sum=c+d;
+        
+        System.out.println(+sum);
+        
+    }
+    
+}
+        
+        
+        
+        
+      
